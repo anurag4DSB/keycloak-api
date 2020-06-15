c83e61e7ccc7ce52cc57ba25481c96b64424ef71
Anurag Mittal <anurag.213@gmail.com>
2020-06-15 07:34:38 +0200

    added relm config

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+// 1
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 
 const app = express();
 app.use(bodyParser.json());
+
+// 2
+const memoryStore = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: '3395167c-3c5e-44e1-b088-def2952676fd',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+  })
+);
+
+// 3
+const keycloak = new Keycloak({
+  store: memoryStore
+});
+
+app.use(
+  keycloak.middleware({
+    logout: '/logout',
+    admin: '/'
+  })
+);
 
 app.get('/api/unsecured', function(req, res) {
   res.json({ message: 'This is an unsecured endpoint payload' });
 });
 
-app.get('/api/user', function(req, res) {
+// 4
+app.get('/api/user', keycloak.protect('realm:user'), function(req, res) {
   res.json({ message: 'This is an USER endpoint payload' });
 });
 
-app.get('/api/admin', function(req, res) {
+app.get('/api/admin', keycloak.protect('realm:admin'), function(req, res) {
   res.json({ message: 'This is an ADMIN endpoint payload' });
 });
 
